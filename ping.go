a72c43402dee28ed55786ed8dc0b34077a702089
KforG <34801641+KforG@users.noreply.github.com>
2021-04-08 23:33:07 +0200

    Optimized the pinger and selector, same functionality

--- ping.go
+++ ping.go
@@ -11,10 +11,13 @@ var nodes = [4]string{"fr1.vtconline.org", "p2proxy.vertcoin.org", "p2p-usa.xyz"
 var results = [len(nodes)]time.Duration{}
 
 func main() {
-	closestNode()
+	pingNode()
 }
 
-func pingNode() {
+func pingNode() string {
+	bestNode := nodes[0]
+	lowest := results[0]
+
 	for i := 0; i < len(nodes); i++ {
 		pinger, err := ping.NewPinger(nodes[i])
 		pinger.SetPrivileged(true)  //This line is needed for windows because of ICMP
@@ -34,22 +37,13 @@ func pingNode() {
 			results[i] = 5000000000
 		}
 		fmt.Printf("%s: %v \n", nodes[i], results[i])
-	}
-}
 
-func closestNode() string {
-	pingNode()
-
-	bestNode := nodes[0]
-	lowest := results[0]
-
-	for i := 1; i < len(results); i++ {
 		if results[i] <= lowest {
 			bestNode = nodes[i]
 			lowest = results[i]
 		}
 	}
-	if bestNode == "p2proxy.vertcoin.org" { //Currently this node uses port 9172 instead of 9171, if this changes this statement can be removed and 9171 can be added to all nodes
+	if bestNode == "p2proxy.vertcoin.org" { //Currently this node uses port 9172 instead of 9171, if this changes this statement can be removed and :9171 can be added to all nodes
 		bestNode += ":9172"
 	} else {
 		bestNode += ":9171"
